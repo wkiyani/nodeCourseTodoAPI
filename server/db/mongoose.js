4acc002df3fa2fc572675084fb21603828180ab0
Wasif Kiyani <wasifwk@hotmail.com>
2018-08-30 16:13:02 +0400

    Set up app for heroku deployment

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,6 +3,11 @@ var mongoose = require ('Mongoose');
 //adding promises library to mongoose
 mongoose.Promise = global.Promise;
 //connecting database
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+let db = {
+	localhost: 'mongodb://localhost:27017/TodoApp',
+	mlab: 'mongodb://wasifwk:fullmetal123@ds022228.mlab.com:22228/todoapidb'
+}
+mongoose.connect(db.localhost || db.mlab);
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var {Todo} = require('./models/todo.js');
 var {User} = require('./models/user.js');
 
 var app = express();
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
@@ -47,7 +48,6 @@ app.get('/todos/:id', (req, res) => {
 	});
 });
 
-var port = 3000;
 app.listen(port, () => {
 	console.log('started on port', port);
 })
